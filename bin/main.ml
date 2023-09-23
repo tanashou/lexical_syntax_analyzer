d4377164c2260fb5db8e34fa9e67e560111fb888
tanashou <tanashou0210@gmail.com>
2023-09-23 16:27:05 +0900

    change read line method

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 let main () =
-  let expr = Parser.start Lexer.token (Lexing.from_channel stdin) in (* 入力後，Ctrl + D が必要. TODO: enter のみで入力完了にしたい *)
+  let expr = Parser.start Lexer.token (Lexing.from_string (read_line ())) in
   (* これで標準入力を字句解析して、構文解析した結果を expr に入れ *)
   print_string "Parsed : ";
   Syntax.print expr;            (* 入力を表示する *)
